961e1b557b5c8b8a8986272fe7deeb10bc1447b3
skylerto <skylerclayne@gmail.com>
2019-09-10 17:21:48 -0400

    trigger build
    Signed-off-by: skylerto <skylerclayne@gmail.com>

--- .buildkite/scripts/release.sh
+++ .buildkite/scripts/release.sh
@@ -5,5 +5,6 @@ set -eu
 RELEASE_TYPE=$(buildkite-agent meta-data get release-type)
 
 ## Docs
+## Trigger build
 
 echo "Executing pipeline with values ${RELEASE_TYPE} on commit ${BUILDKITE_COMMIT}"
